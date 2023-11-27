e6cecdb920eda7c1c442df9f63a9d0dbb3a11bf7
Nutifafa Afi Attor <attorfafa@gmail.com>
2023-11-27 15:41:08 +0000

    add account suspended template

--- src/templates/mails/manager.ts
+++ src/templates/mails/manager.ts
@@ -14,5 +14,9 @@ export default {
     ManagerLoggedIn: {
         subject: lodash.template("Account Login"),
         message: lodash.template("Dear ${user.othernames} ${user.surname},\n\nWe just detected a recent login into your account. If this was you, you can safely ignore this email, otherwise you should change your password immediately.")
+    },
+    ManagerAccountSuspended: {
+        subject: lodash.template("Account Suspension"),
+        message: lodash.template("Dear ${user.othernames} ${user.surname},\n\nYour account has been suspended. Kindly contact your managers to follow up. Until your account is unsuspended, you will not be able to use your account.")
     }
 }
